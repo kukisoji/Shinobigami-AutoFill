63e1c1d3904262162529ca04025a398fd3061b3a
クキソージ <gorira1802@gmail.com>
2024-10-19 00:22:14 +0900

    ver2.0

--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
   const searchBtn = document.getElementById('search-btn');
-  if (searchBtn) {
-    searchBtn.addEventListener('click', async function () {
-      console.log("作業開始ぃ！");
+  const newActionBtn = document.getElementById('gaba-check');
+  let isScriptInjected = false;
 
-      // アクティブなタブを取得
-      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  async function sendMessageToContentScript(action) {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-      // コンテントスクリプトをアクティブなタブにインジェクト
+    if (!isScriptInjected) {
       try {
         await chrome.scripting.executeScript({
           target: { tabId: tab.id },
           files: ['content-scripts/content.js']
         });
+        isScriptInjected = true;
       } catch (error) {
         console.error('スクリプトのインジェクトにエラー:', error);
       }
+    }
 
-      // コンテントスクリプトにメッセージを送信
-      try {
-        chrome.tabs.sendMessage(tab.id, { action: 'searchArrayAndOutput' });
-      } catch (error) {
-        console.error('メッセージの送信にエラー:', error);
-        // エラーを処理する、例えばスクリプトをインジェクトして再試行する
+    try {
+      chrome.tabs.sendMessage(tab.id, { action: action });
+    } catch (error) {
+      console.error('メッセージの送信にエラー:', error);
+      // スクリプトがインジェクトされていない場合、再度インジェクトしてメッセージを送信
+      if (!isScriptInjected) {
         await chrome.scripting.executeScript({
           target: { tabId: tab.id },
           files: ['content-scripts/content.js']
         });
-        chrome.tabs.sendMessage(tab.id, { action: 'searchArrayAndOutput' });
+        isScriptInjected = true;
+        chrome.tabs.sendMessage(tab.id, { action: action });
       }
+    }
+  }
+
+  if (searchBtn) {
+    searchBtn.addEventListener('click', async function () {
+      console.log("検索と出力開始！");
+      await sendMessageToContentScript('searchArrayAndOutput');
     });
   }
+
+  if (newActionBtn) {
+    newActionBtn.addEventListener('click', async function () {
+      await sendMessageToContentScript('GabaCheckStart');
+    });
+  }
+
+  // ページリロード時にフラグをリセット
+  window.addEventListener('beforeunload', () => {
+    isScriptInjected = false;
+  });
 });
